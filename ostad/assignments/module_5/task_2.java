92eeb8bfba010e8d7fd09439f716c53c917c9303
Ahmad61-6 <ahmadmizan210@gmail.com>
2025-12-17 23:48:07 +0600

    solved module 5 task 2

--- ostad/assignments/module_5/task_2.java
+++ ostad/assignments/module_5/task_2.java
@@ -1,27 +1,37 @@
 import java.util.HashMap;
-import java.util.Map;
 
 public class task_2 {
     public static void encrypt_decrypt(String str){
-        Map<Character, Integer> charCount = new HashMap<Character,Integer>();
-        for(int i = 0 ; i < str.length(); i++){
-
-            char ch = str.charAt(i);
-            if(charCount.containsKey(ch)){
-                charCount.put(ch, charCount.get(ch)+1);
+ 
+        HashMap<Character,Integer> encryptMap = new HashMap<>();
+         for(char c : str.toCharArray()){
+            if (encryptMap.containsKey(c)) {
+                encryptMap.put(c, encryptMap.get(c)+1);
+                
             }
             else{
-                charCount.put(ch
-                    , 1);
+                encryptMap.put(c, 1);
             }
-        } 
-        System.out.println(charCount);
-        StringBuilder sb = new StringBuilder();
-        
+         }
+         System.out.println(encryptMap);
+         String reverse = new StringBuilder(str).reverse().toString();
+         String dec = "";
+         System.out.println("a"+1);
+
+        //  for(int i = 0;i<str.length();i++){
+        //     char c = str.charAt(i);
+        //     encryptMap.get
+        //  }
+        StringBuilder decBuilder = new StringBuilder();
+        for(int i = 0 ; i< reverse.length(); i++){
+            char c = reverse.charAt(i);
+           decBuilder.append(encryptMap.get(c)).append(c);
+        }
+        System.out.println("Decrypted String: "+decBuilder.toString());
     
     }
     public static void main(String[] args) {
-        String str = "helloworld";
+        String str = "ostad";
         encrypt_decrypt(str);
     }
     
